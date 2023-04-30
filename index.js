418993a12d5435be0699e0c84671862c39c961dd
Laina Nguyen <78044530+lainanguyen@users.noreply.github.com>
2023-04-30 18:44:10 -0400

    install logrocket

---
+++ index.js
@@ -0,0 +1,19 @@
+// import LogRocket from 'logrocket';
+
+// LogRocket.init('91kzhe/demo');
+
+// LogRocket.identify('Bob_10101', {
+//     name: 'Bob',
+//     email: 'bob@gmail.com',
+//     // custom variable
+//     age: 21,
+// });
+
+// LogRocket.identify('Alice_20202', {
+//     name: 'Alice',
+//     email: 'alice@example.com',
+//     age: 27,
+// });
+
+
+
